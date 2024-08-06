1a4100cbdb6fd910c91f98b477e4e92f0d34f812
Asc <asc027SS@gmail.com>
2024-08-06 00:10:29 -0500

    Create time.hpp

---
+++ Calendar/time.hpp
@@ -0,0 +1,13 @@
+    std::time_t t = std::time(nullptr);
+
+    std::tm* localTime = std::localtime(&t);
+    int month = localTime->tm_mon + 1;
+
+    int dayOfMonth = localTime->tm_mday;
+    int dayOfWeek = localTime->tm_wday;
+
+    int hour = localTime->tm_hour;
+    int minute = localTime->tm_min;
+    std::string newMinute = std::to_string(minute);
+
+    int year = localTime->tm_year + 1900;
