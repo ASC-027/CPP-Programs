aafcaa1f509c0c7b56412951759d86a442e1ec6e
Asc <asc027SS@gmail.com>
2024-08-07 17:46:46 -0500

    Update time.hpp

--- Calendar/time.hpp
+++ Calendar/time.hpp
@@ -1,13 +1,19 @@
-    std::time_t t = std::time(nullptr);
-    std::tm* localTime = std::localtime(&t);
+#ifndef TIME_HPP
+#define TIME_HPP
 
-    int month = localTime->tm_mon + 1;
+#include <ctime>
 
-    int dayOfMonth = localTime->tm_mday;
-    int dayOfWeek = localTime->tm_wday;
+std::time_t t = std::time(nullptr);
+std::tm* localTime = std::localtime(&t);
 
-    int hour = localTime->tm_hour;
-    int minute = localTime->tm_min;
-    std::string newMinute = std::to_string(minute);
+int month = localTime->tm_mon + 1;
+int dayOfMonth = localTime->tm_mday;
+int dayOfWeek = localTime->tm_wday;
 
-    int year = localTime->tm_year + 1900;
+int hour = localTime->tm_hour;
+int minute = localTime->tm_min;
+std::string newMinute = std::to_string(minute);
+
+int year = localTime->tm_year + 1900;
+
+#endif TIME_HPP
